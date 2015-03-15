15e6f1c7dc1d864a6a40ce6369509475dd9760b1
Evan Sullivan <evan@grabcad.com>
2015-03-15 15:57:30 -0400

    Working towards date range functionality. I seem to have broken sorting with this submission, but want to get the code up.

--- parser.js
+++ parser.js
@@ -23,6 +23,31 @@
  */
 ////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////
+// Class definitions.
+var CalendarEvent = function (summary, start, end) {
+    this.summary = summary;
+    this.start = start;
+    this.end = end;
+};
+
+function DateTime(year, month, day, hour, minute) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+    this.hour = hour;
+    this.minute = minute;
+};
+DateTime.prototype.print = function() {
+    return this.month + "/" +
+        this.day + "/" +
+        this.year + " at " +
+        this.hour + ":" +
+        this.minute;
+}
+
+////////////////////////////////////////////////////
+// Core program logic.
 var fs = require('fs');
 var assert = require('assert');
 var q = require('q');
@@ -35,12 +60,6 @@ var listOfCalendars = [
     "/Calendars to parse/Miss Ashley Art.ics",
     "/Calendars to parse/Work.ics"];
 var calendarCount = listOfCalendars.length;
-
-var CalendarEvent = function (summary, start, end) {
-    this.summary = summary;
-    this.start = start;
-    this.end = end;
-};
 
 var allCalendarEvents = [];
 
@@ -68,7 +87,7 @@ var promise = q.Promise(function(resolve, reject, notify) {
                     lastLineRead = "DTEND";
 
                     encounteredFirstEnd = true;
-                    end = line.substring(line.indexOf(':') + 1, line.length);
+                    end = getDateTime(line);
                 }
 
                 if (encounteredFirstEnd == true) {
@@ -86,41 +105,112 @@ var promise = q.Promise(function(resolve, reject, notify) {
                         assert(lastLineRead == "SUMMARY");
                         lastLineRead = "DTSTART";
 
-                        start = line.substring(line.indexOf(':') + 1, line.length);
+                        start = getDateTime(line);
 
                         var newCalendarEvent = new CalendarEvent(summary, start, end);
                         allCalendarEvents[allCalendarEvents.length] = newCalendarEvent;
                     }
                 }
             });
-        });
 
-        calendarCount--;
-        if (calendarCount == 0) {
-            resolve();
-        }
+            // Decrement how many calendars we're waiting on. If this was the last one, resolve the promise.
+            calendarCount--;
+            if (calendarCount == 0) {
+                resolve();
+            }
+        });
     });
 });
 
 promise.then(
     function onFulfilled() {
         // Promise kept. :)
-        var sortedEvents = {};
-        for (var i = 0; i < allCalendarEvents.length; i++) {
-            sortedEvents[allCalendarEvents[i].start] = allCalendarEvents[i];
-        }
+        console.log("\nFor Dear.");
+        console.log("Love, Handsome.\n");
 
-        var count = 1;
-        for (var event in sortedEvents) {
-            console.log("Event " + count);
-            console.log("\tSummary: " + event.summary);
-            console.log("\tStart: " + event.start);
-            console.log("\tEnd: " + event.end);
-            count++;
-        }
+        sortCalendars();
+        removeOutsideDateRange(new DateTime(2014, 01, 01, 00, 00), new DateTime(2015, 01, 01, 00, 00));
+        printCalendars();
     },
     function onRejected(error) {
         // Promise broken. :(
         console.log("error");
     }
 );
+
+////////////////////////////////////////////////////
+// Functions
+function getDateTime(lineIn)
+{
+    var sub = lineIn.substring(lineIn.indexOf(':') + 1, lineIn.length);
+    var endYear = Number(sub.substring(0, 4));
+    var endMonth = Number(sub.substring(4, 6));
+    var endDay = Number(sub.substring(6, 8));
+    var endHour = 0;
+    var endMinute = 0;
+    if (sub.length >= 13) {
+        endHour = Number(sub.substring(9, 11));
+        endMinute = Number(sub.substring(11, 13));
+    }
+    return new DateTime(endYear, endMonth, endDay, endHour, endMinute);
+}
+
+// This functions assumes allCalendarEvents is sorted in ascending order by start dateTime.
+function removeOutsideDateRange(rangeStart, rangeEnd) {
+    var startIndex = 0;
+    var endIndex = allCalendarEvents.length - 1;
+
+    for (var i = 0; i < allCalendarEvents.length - 1; i++) {
+        // If an event ends after the rangeStart cutoff, it's included.
+        var startCompare = compareDateTimes(allCalendarEvents[i].end, rangeStart);
+        if (startCompare >= 0) {
+            startIndex = i;
+            break;
+        }
+    }
+
+    for (var i = startIndex; i < allCalendarEvents.length - 1; i++) {
+        // If an event starts before the rangeEnd cutoff, it's included.
+        var endCompare = compareDateTimes(allCalendarEvents[i].start, rangeEnd);
+        if (endCompare <= 0) {
+            endIndex = i;
+        }
+    }
+
+    allCalendarEvents = allCalendarEvents.subarray(startIndex, endIndex);
+}
+
+function sortCalendars() {
+    allCalendarEvents.sort(function (x, y) {
+        compareDateTimes(x.start, y.start);
+    });
+};
+
+// Returns positive if date1 is after date2, negative if date2 is after date1, and 0 otherwise.
+function compareDateTimes(date1, date2) {
+    if (date1.year == date2.year) {
+        if (date1.month == date2.month) {
+            if (date1.day == date2.day) {
+                if (date1.hour == date2.hour) {
+                    if (date1.minute == date2.minute) {
+                        return 0;
+                    }
+                    return date1.minute - date2.minute;
+                }
+                return date1.hour - date2.hour;
+            }
+            return date1.day - date2.day;
+        }
+        return date1.month - date2.month;
+    }
+    return date1.year - date2.year;
+}
+
+function printCalendars() {
+    for (var i = 0; i < allCalendarEvents.length; i++) {
+        console.log("Event " + i);
+        console.log("\tSummary: " + allCalendarEvents[i].summary);
+        console.log("\tStart: " + allCalendarEvents[i].start.print());
+        console.log("\tEnd: " + allCalendarEvents[i].end.print());
+    }
+};
